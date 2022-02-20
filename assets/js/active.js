d278c9ab198f3ff92406580742db8b70d3a4b745
mdismail645221 <mdismail645221@gmail.com>
2022-02-20 11:53:27 +0600

    this is my programming hero job is first commit

---
+++ assets/js/active.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function(){
+  $(".slide-wrapper").owlCarousel({
+  	items: 1,
+  	dots: true,
+  	margin: 30,
+  });
+});*/
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items:1,
+  	loop: false,
+  	dots: true,
+  });
+});
